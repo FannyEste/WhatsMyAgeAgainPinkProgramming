eb7279c3dfe39db1fcad5e9e72d848d347432250
Estefanny Moran <estefannymorana@gmail.com>
2025-02-09 14:19:16 +0100

    added name and email

--- pink-feb.js
+++ pink-feb.js
@@ -2,10 +2,12 @@
 function greetUser() {
     let name = prompt("What is your name?");
     let age = parseInt(prompt("How old are you?"));
+    let myName = "Your Name";
+    let myEmail = "your.email@example.com";
     
     if (!isNaN(age)) {
         let futureAge = age + 5;
-        alert(`Hello, ${name}! In 5 years, you will be ${futureAge} years old.`);
+        alert(`Hello, ${name}! In 5 years, you will be ${futureAge} years old. My name is ${myName}, and you can contact me at ${myEmail}.`);
     } else {
         alert("Please enter a valid age.");
     }
